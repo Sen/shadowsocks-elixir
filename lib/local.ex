a87485d0314aacf6f389c3daa1d3a9deaf0db6c8
Sen <sen9ob@gmail.com>
2014-01-07 01:28:22 +0800

    rewrite code to use {active, once} option

--- lib/local.ex
+++ lib/local.ex
@@ -1,28 +1,32 @@
 defmodule Local do
   def listen(port) do
-    tcp_options = [:binary, {:packet, 0}, {:active, false}, {:reuseaddr, true}]
+    tcp_options = [:binary, {:packet, 0}, {:active, :once}, {:reuseaddr, true}]
     {:ok, l_socket} = :gen_tcp.listen(port, tcp_options)
     do_listen(l_socket)
   end
 
   def do_listen(l_socket) do
     {:ok, socket} = :gen_tcp.accept(l_socket)
-    spawn(fn() -> do_server(socket) end)
+    pid = spawn(fn() -> do_server(socket) end)
+    :gen_tcp.controlling_process(socket, pid)
     do_listen(l_socket)
   end
 
   def do_server(socket) do
-    case :gen_tcp.recv(socket, 0) do
-      { :ok, << 5, 1, 0 >> } ->
+    :inet.setopts(socket, [{:active, :once}])
+    receive do
+      {:tcp, socket, << 5, 1, 0 >>} ->
         :ok = :gen_tcp.send(socket, << 5, 0 >>)
 
         do_server(socket)
-      { :ok, data } ->
+      {:tcp, socket, data} ->
         << _ver, cmd, _rsv, address_type, rest :: binary >> = data
         case cmd do
           1 -> # scucess
-            {:ok, remote_socket} = :gen_tcp.connect('localhost', 8388, [:binary, {:packet, 0}, {:active, false}])
-            spawn_link(fn() -> do_remote(remote_socket, socket) end)
+            {:ok, remote_socket} = :gen_tcp.connect('localhost', 8388,
+              [:binary, {:packet, 0}, {:active, :once}])
+            pid = spawn_link(fn() -> do_remote(remote_socket, socket) end)
+            :gen_tcp.controlling_process(remote_socket, pid)
             remote_sender_pid = spawn_link(fn() -> do_remote_sender(remote_socket) end)
 
             case address_type do
@@ -42,8 +46,10 @@ defmodule Local do
           _ ->
             :ok = :gen_tcp.send(socket, << 5, 7, 0, 1 >>)
             :ok = :gen_tcp.close(socket)
-            Process.exit(self, :exit)
         end
+
+      {:tcp_closed, socket} ->
+        :ok
 
       _ ->
         :ok = :gen_tcp.close(socket)
@@ -51,13 +57,13 @@ defmodule Local do
   end
 
   def do_server(socket, remote_sender_pid) do
-    case :gen_tcp.recv(socket, 0) do
-      { :ok, data } ->
+    :inet.setopts(socket, [{:active, :once}])
+    receive do
+      {:tcp, socket, data} ->
         remote_sender_pid <- { :send, data }
         do_server(socket, remote_sender_pid)
 
-      { :error, :closed } ->
-        Process.exit(self, :exit)
+      {:tcp_closed, socket} ->
         :ok
     end
   end
@@ -65,21 +71,27 @@ defmodule Local do
   def do_remote_sender(socket) do
     receive do
       { :send, data } ->
-        :ok = :gen_tcp.send(socket, data)
-        do_remote_sender(socket)
+        case :gen_tcp.send(socket, data) do
+          :ok ->
+            do_remote_sender(socket)
+          {:error, :closed} ->
+            :ok
+        end
     end
   end
 
   def do_remote(remote_socket, socket) do
-    case :gen_tcp.recv(remote_socket, 0) do
-      { :ok, data } ->
-        :ok = :gen_tcp.send(socket, data)
-        do_remote(remote_socket, socket)
+    :inet.setopts(remote_socket, [{:active, :once}])
+    receive do
+      {:tcp, remote_socket, data} ->
+        case :gen_tcp.send(socket, data) do
+          :ok ->
+            do_remote(remote_socket, socket)
+          {:error, :closed } ->
+            :ok
+        end
 
-      { :error, :closed } ->
-        :ok = :gen_tcp.close(remote_socket)
-        :ok = :gen_tcp.close(socket)
-
+      {:tcp_closed, remote_socket} ->
         :ok
     end
   end
--- lib/server.ex
+++ lib/server.ex
@@ -1,19 +1,21 @@
 defmodule Server do
   def listen(port) do
-    tcp_options = [:binary, {:packet, 0}, {:active, false}, {:reuseaddr, true}]
+    tcp_options = [:binary, {:packet, 0}, {:active, :once}, {:reuseaddr, true}]
     {:ok, l_socket} = :gen_tcp.listen(port, tcp_options)
     do_listen(l_socket)
   end
 
   def do_listen(l_socket) do
     {:ok, socket} = :gen_tcp.accept(l_socket)
-    spawn(fn() -> do_server(socket) end)
+    pid = spawn(fn() -> do_server(socket) end)
+    :gen_tcp.controlling_process(socket, pid)
     do_listen(l_socket)
   end
 
   def do_server(socket) do
-    case :gen_tcp.recv(socket, 0) do
-      { :ok, data } ->
+    :inet.setopts(socket, [{:active, :once}])
+    receive do
+      {:tcp, socket, data} ->
         << addr_type, addr_len, rest :: binary >> = data
 
         case addr_type do
@@ -28,8 +30,9 @@ defmodule Server do
             :ok = :gen_tcp.close(socket)
         end
 
-        {:ok, request_socket} = :gen_tcp.connect(remote_addr_list, remote_port, [:binary, {:packet, 0}, {:active, false}])
-        spawn_link(fn() -> do_request(request_socket, socket) end)
+        {:ok, request_socket} = :gen_tcp.connect(remote_addr_list, remote_port, [:binary, {:packet, 0}, {:active, :once}])
+        pid = spawn_link(fn() -> do_request(request_socket, socket) end)
+        :gen_tcp.controlling_process(request_socket, pid)
         request_sender_pid = spawn_link(fn() -> do_request_sender(request_socket) end)
 
         if String.length(to_send) > 0 do
@@ -37,34 +40,42 @@ defmodule Server do
         end
 
         do_server(socket, request_sender_pid)
-      { :error, :closed } -> :ok
+      {:tcp_closed, socket} -> :ok
     end
   end
 
   def do_server(socket, request_sender_pid) do
-    case :gen_tcp.recv(socket, 0) do
-      { :ok, data } ->
+    :inet.setopts(socket, [{:active, :once}])
+    receive do
+      {:tcp, socket, data} ->
         request_sender_pid <- { :send, data }
         do_server(socket, request_sender_pid)
-      { :error, :closed } -> :ok
+      {:tcp_closed, socket} -> :ok
     end
   end
 
   def do_request_sender(socket) do
     receive do
       { :send, data } ->
-        :ok = :gen_tcp.send(socket, data)
-        do_request_sender(socket)
+        case :gen_tcp.send(socket, data) do
+          :ok ->
+            do_request_sender(socket)
+          { :error, :closed } -> :ok
+        end
     end
   end
 
   def do_request(request_socket, socket) do
-    case :gen_tcp.recv(request_socket, 0) do
-      { :ok, data } ->
-        :ok = :gen_tcp.send(socket, data)
-        do_request(request_socket, socket)
+    :inet.setopts(request_socket, [{:active, :once}])
+    receive do
+      {:tcp, request_socket, data} ->
+        case :gen_tcp.send(socket, data) do
+          :ok ->
+            do_request(request_socket, socket)
+          { :error, :closed } -> :ok
+        end
 
-      { :error, :closed } -> :ok
+      {:tcp_closed, request_socket} -> :ok
     end
   end
 
